1eaa44d1f9b21934b36339ba323ca20bcbbfe49e
charly <kouasseu.tamayeu@gmail.com>
2025-06-18 08:06:24 +0100

    commencement de l'interface d'insertion d'un nouveau étudiant.

--- src/views/StudentAddView.vue
+++ src/views/StudentAddView.vue
@@ -1,4 +1,32 @@
 <template>
-    <div class="">
+    <div class="flex flex-col gap-5">
+		<!-- bare de recherche, titre, et bouton ajouter -->
+		<header class="w-full bg-base-100 border border-base-300 flex justify-between items-center p-3 rounded-box">
+
+			<!-- title -->
+			<h1 class="text-xl text-nowrap">Nouveau Etudiants</h1>
+
+			<div class="flex gap-3 items-center">
+				
+			</div>
+		</header>
+
+		<div class="flex">
+
+			<!-- info personnel -->
+			<div class="w-[50%]">
+
+				<!-- l'image -->
+				<div class="w-full h-[100px] bg-base-100 p-(--padding-box) rounded-box border border-base-300">
+					<div class="w-full h-full rounded-box border border-inherit border-dashed"></div>
+				</div>
+
+			</div>
+
+			<!-- info inscription -->
+			<div>
+
+			</div>
+		</div>
     </div>
 </template>
